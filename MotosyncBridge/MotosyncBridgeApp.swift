8927e3c4d79fb89c0d92386c42488e8721ef1b22
Priyajit <npriyajit1@gmail.com>
2026-06-04 11:28:03 +0530

    Initial Commit

---
+++ MotosyncBridge/MotosyncBridgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MotosyncBridgeApp.swift
+//  MotosyncBridge
+//
+//  Created by Priyajit Nayak on 04/06/26.
+//
+
+import SwiftUI
+
+@main
+struct MotosyncBridgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
